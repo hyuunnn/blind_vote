7d08a9098c785370f54c6a02931c1f63d4f5c836
Hyun Yi <hy00un__@naver.com>
2024-06-25 17:04:53 +0900

    Update create_test_account.py

--- create_test_account.py
+++ create_test_account.py
@@ -14,21 +14,25 @@ cursor.execute(
     )"""
 )
 
-test_username = "user"
-test_password = "password"
-hashed_password = hashpw(test_password.encode("utf-8"), gensalt()).decode("utf-8")
+test_accounts = [
+    ("user1", "password1"),
+    ("user2", "password2"),
+    ("user3", "password3"),
+]
 
-try:
-    cursor.execute(
-        """
-        INSERT INTO user (username, password, is_voted)
-        VALUES (?, ?, ?)
-        """,
-        (test_username, hashed_password, False),
-    )
-    print(f"Test user '{test_username}' created successfully.")
-except sqlite3.IntegrityError:
-    print(f"User '{test_username}' already exists.")
+for username, password in test_accounts:
+    hashed_password = hashpw(password.encode("utf-8"), gensalt()).decode("utf-8")
+    try:
+        cursor.execute(
+            """
+            INSERT INTO user (username, password, is_voted)
+            VALUES (?, ?, ?)
+            """,
+            (username, hashed_password, False),
+        )
+        print(f"Test user '{username}' created successfully.")
+    except sqlite3.IntegrityError:
+        print(f"User '{username}' already exists.")
 
 conn.commit()
 conn.close()
