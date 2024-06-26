4a95ba62b69b44a7f812e2f6448dd4b70d38a2be
Hyun Yi <hy00un__@naver.com>
2024-06-26 17:07:12 +0900

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from bcrypt import checkpw
+from werkzeug.security import check_password_hash
 
 db = SQLAlchemy()
 
@@ -13,7 +14,12 @@ class User(UserMixin, db.Model):
     is_voted = db.Column(db.Boolean, default=False)
 
     def check_password(self, password):
-        return checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
+        try:
+            return checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
+        except ValueError as e:
+            return check_password_hash(self.password, password)
+        except Exception as e:
+            raise Exception(f"비밀번호 확인 중 예외 발생: {str(e)}")
 
     def __repr__(self):
         return f"<User {self.username}>"
